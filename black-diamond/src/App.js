c434239b51cfebe7a5540ba32a998abf53e149cb
HAL_9000 <jessebadgley@HAL-9000.local>
2019-10-23 15:11:26 -0500

    commited

--- black-diamond/src/App.js
+++ black-diamond/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+
 import StoreFront from "./Components/StoreFront/StoreFront";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
@@ -12,10 +13,15 @@ class App extends Component {
       cart: [],
       showCart: false
     };
+
+    this.addToCart = this.addToCart.bind(this);
+    this.removeFromCart = this.removeFromCart.bind(this);
+    this.navigate = this.navigate.bind(this);
+
   }
   componentDidMount() {
     axios
-      .get("https://practiceapi.devmountain.com/products/")
+    .get("https://practiceapi.devmountain.com/products/")
       .then(response => {
         this.setState({
           products: response.data
@@ -36,9 +42,9 @@ class App extends Component {
   }
   navigate(location) {
     if (location === "cart") {
-      this.state.showCart = true;
+      this.setState({showCart: true})
     } else {
-      this.state.showCart = false;
+      this.setState({showCart: false})
     }
   }
   render() {
